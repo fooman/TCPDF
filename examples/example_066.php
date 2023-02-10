09eeb279bac68868edd694182973c2bcfd21563e
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2023-02-10 16:10:26 +1300

    include require in example66

--- examples/example_066.php
+++ examples/example_066.php
@@ -19,7 +19,8 @@
  */
 
 // Load the autoloader, move one folder back from examples
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once(__DIR__.'/../vendor/autoload.php');
+require_once (__DIR__ .'/config/tcpdf_config_alt.php');
 
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false, true);
