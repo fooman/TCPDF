adba3235ffd1f361cde3f2ed2765b7e30e6e83b7
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2025-02-12 14:33:06 +1300

    remove pull log output

--- tests/launch.sh
+++ tests/launch.sh
@@ -126,7 +126,7 @@ for file in $EXAMPLE_FILES; do
             echo "Generated-invalid-file: $file"
         fi
         if [ "$file" = "examples/example_065.php" ] || [ "$file" = "examples/example_066.php" ]; then
-          VALIDATION_OUTPUT="$(docker run -v $TEMP_FOLDER:/data --rm -w /data/ pdfix/verapdf-validation:latest validate --format 'json' -i 'output.pdf')"
+          VALIDATION_OUTPUT="$(docker run -v $TEMP_FOLDER:/data --quiet --rm -w /data/ pdfix/verapdf-validation:latest validate --format 'json' -i 'output.pdf')"
           VALIDATION_RESULT="$(echo $VALIDATION_OUTPUT |  jq '.report.jobs[0].validationResult[0].compliant')"
           if [ "$VALIDATION_RESULT" = "false" ]; then
               FAILED_FLAG=1
